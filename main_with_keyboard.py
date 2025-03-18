eb438e754f2eaafca21d699149d430696cc4cdeb
PatimaKruafu <patima.krua@kmutt.ac.th>
2025-03-18 12:58:06 +0700

    tuning raycasting, added drawing mode, cleanup
    remove unnecessary code such as keyboard binds

--- main_with_keyboard.py
+++ main_with_keyboard.py
@@ -3,7 +3,6 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 from OpenGL.GLUT.fonts import GLUT_BITMAP_HELVETICA_18
 import numpy as np
-from numpy import interp
 import math
 import pickle
 
@@ -22,6 +21,9 @@ layer = 0
 
 CUBE_SIZE = 1
 CUBE_OFFSET = CUBE_SIZE/2
+
+# Cursor position for keyboard
+cursor_x, cursor_y, cursor_z = GRID_SIZE // 2, 0, GRID_SIZE // 2  # Start in the middle of the grid
 
 #--------------------Save And Load Function--------------------
 # Dictionary to store multiple grid states
@@ -77,7 +79,7 @@ def init():
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     #gluPerspective(45, window_width / window_height, 0.1, 50.0)
-    glOrtho(-GRID_SIZE, GRID_SIZE, -GRID_SIZE, GRID_SIZE, -10 * GRID_SIZE, 10 * GRID_SIZE)
+    glOrtho(-GRID_SIZE, GRID_SIZE, -GRID_SIZE, GRID_SIZE, -100, 100)
     glMatrixMode(GL_MODELVIEW)
     glEnable(GL_DEPTH_TEST)
     glClearColor(0.2, 0.3, 0.4, 1.0)  # Background color
@@ -100,8 +102,6 @@ def draw_grid():
         glVertex3f(HALF_GRID, 0, i)
     glEnd()
 
-    #this causes lag because too much square is being drawn at once
-    #change into a small circle around cursor seems to be good
     glColor4f(0.0, 0.0, 0.0, 0.5)
     for i in range(-HALF_GRID, HALF_GRID):
         for j in range(-HALF_GRID, HALF_GRID):
@@ -132,21 +132,6 @@ def draw_reference_line():
     glVertex3f(-HALF_GRID, HALF_GRID, -HALF_GRID)
     glEnd()
 
-# Check if a block is present at the given coordinates
-def is_block_present(x, y, z):
-    if 0 <= x < GRID_SIZE and 0 <= y < GRID_SIZE and 0 <= z < GRID_SIZE:
-        return grid[x][y][z] == 1
-    return True
-
-def cubeColor():
-    global draw_mode_num
-        # change color based on draw modes
-    match draw_mode_num:
-        case 0:
-            glColor3f(232/255, 66/255, 32/255)
-        case 1:
-            glColor3f(222/255, 222/255, 47/255)
-
 # Draw cubes based on the 3D grid
 def draw_blocks():
     for x in range(GRID_SIZE):
@@ -157,51 +142,80 @@ def draw_blocks():
 
 # Draw a cube at a specified position
 def draw_cube(x, y, z, highlight=False):
-    glColor3f(79/255, 194/255, 152/255) #cube color
-
     glPushMatrix()
-    glTranslatef(x + CUBE_OFFSET, y + CUBE_OFFSET, z + CUBE_OFFSET)   
-    glutSolidCube(CUBE_SIZE)
-    glPopMatrix()
-
-    glPushMatrix()
-    glTranslatef(x + CUBE_OFFSET, y + (CUBE_OFFSET * 2), z + CUBE_OFFSET)   
-    glutSolidCube(CUBE_SIZE)
-    glPopMatrix()
-
-def cursor_cube(x, y, z, drawMode):
-    cubeColor()
-    glPushMatrix()
-    glTranslatef(x + CUBE_OFFSET, y + CUBE_OFFSET, z + CUBE_OFFSET)   
-    glutSolidCube(CUBE_SIZE)
-    glPopMatrix()
-
-    glPushMatrix()
-    glTranslatef(x + CUBE_OFFSET, y + 2*CUBE_OFFSET, z + CUBE_OFFSET)   
+    glTranslatef(x + CUBE_OFFSET, y + CUBE_OFFSET, z + CUBE_OFFSET)
+    if highlight:
+        glColor3f(222/255, 222/255, 47/255)  # Highlight color for the cursor
+    else:
+        glColor3f(79/255, 194/255, 152/255)
     glutSolidCube(CUBE_SIZE)
     glPopMatrix()
 
 def draw_block_function():
-    global draw_mode_num
-    #cursor
-    if grid_pos is not None:
-        cursor_cube(grid_pos[0] - GRID_SIZE // 2, grid_pos[1], grid_pos[2] - GRID_SIZE // 2, draw_mode_num)
-
     draw_grid()
     draw_blocks()
+    #draw_cube(cursor_x - GRID_SIZE // 2, cursor_y, cursor_z - GRID_SIZE // 2, highlight=True)
+    if grid_pos is not None:
+        draw_cube(grid_pos[0] - GRID_SIZE // 2, grid_pos[1], grid_pos[2] - GRID_SIZE // 2, highlight=True)
 
-    #if eye_position != new_eye_position:
-        
-def place_block(x, y, z):
-    global draw_mode_num
-    if draw_mode_num == 0:  # Remove block
-        grid[grid_pos[0], grid_pos[1], grid_pos[2]] = 0
-    elif draw_mode_num == 1:  # Place block
-        grid[grid_pos[0], grid_pos[1], grid_pos[2]] = 1
 
 #--------------------Draw Blocks----------------------
 
+#--------------------Ortho----------------------------
+# Draw block in orthographic projection
+def draw_block_ortho():
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    aspect_ratio = (window_width // 2) / (window_height // 2)
+    if aspect_ratio > 1:
+        glOrtho(-GRID_SIZE * aspect_ratio, GRID_SIZE * aspect_ratio, -GRID_SIZE, GRID_SIZE, -GRID_SIZE, GRID_SIZE)
+    else:
+        glOrtho(-GRID_SIZE, GRID_SIZE, -GRID_SIZE / aspect_ratio, GRID_SIZE / aspect_ratio, -GRID_SIZE, GRID_SIZE)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
 
+    # Front view
+    glViewport(0, window_height // 2, window_width // 2, window_height // 2)
+    glLoadIdentity()
+    draw_block_function()
+    draw_text(10, window_height // 2 + 20, "Front View")
+
+    # Top view
+    glViewport(window_width // 2, window_height // 2, window_width // 2, window_height // 2)
+    glLoadIdentity()
+    glRotatef(90, 1, 0, 0)
+    draw_block_function()
+    draw_text(window_width // 2 + 10, window_height // 2 + 20, "Top View")
+
+    # Left side view
+    glViewport(0, 0, window_width // 2, window_height // 2)
+    glLoadIdentity()
+    glRotatef(90, 0, 1, 0)
+    draw_block_function()
+    draw_text(10, window_height // 2 - window_height // 2 + 20, "Left Side View")
+
+    # Right side view
+    glViewport(window_width // 2, 0, window_width // 2, window_height // 2)
+    glLoadIdentity()
+    glRotatef(-90, 0, 1, 0)
+    draw_block_function()
+    draw_text(window_width // 2 + 10, window_height // 2 - window_height // 2 + 20, "Right Side View")
+
+    # Save block data to 3D list
+    for x in range(GRID_SIZE):
+        for y in range(GRID_SIZE):
+            for z in range(GRID_SIZE):
+                if is_block_present(x, y, z):
+                    #block_data[x][y][z] = 1
+                    grid[x][y][z] = 1
+
+# Check if a block is present at the given coordinates
+def is_block_present(x, y, z):
+    if 0 <= x < GRID_SIZE and 0 <= y < GRID_SIZE and 0 <= z < GRID_SIZE:
+        return grid[x][y][z] == 1
+    return True
+
+#--------------------Ortho----------------------------
 
 
 
@@ -223,12 +237,11 @@ def get_ray_from_mouse(x, y):
 
 def map_to_new_range(value, old_min, old_max, new_min, new_max):
     # Linear transformation formula
-    #return new_min + (value - old_min) * (new_max - new_min) / (old_max - old_min)
-    return np.interp(value, [old_min, old_max], [new_min, new_max])
+    return new_min + (value - old_min) * (new_max - new_min) / (old_max - old_min)
 
 # Calculate grid position from mouse coordinates
 def get_grid_position(mouse_x, mouse_y):
-    global layer, CUBE_SIZE
+    global layer
     ray = get_ray_from_mouse(mouse_x, mouse_y)
     if ray is None:
         return None
@@ -236,35 +249,16 @@ def get_grid_position(mouse_x, mouse_y):
     if ray[1] == 0:
         return None
     t = -ray[1] / ray[1]
-    
-    #grid_x = round(ray[0] * t)
-    grid_x = math.floor(ray[0] * t)
+
+    grid_x = round(ray[0] * t)
     grid_y = layer
-    grid_z = round( (ray[2] * t) - CUBE_SIZE / 4 ) 
+    grid_z = round(ray[2] * t)
 
     """ if grid_pos is not None:
         grid_y = grid_pos[1]
     else:
         grid_y = 0 """
     
-    #debug
-    # debug1 = ray[2] * t
-    # debug2 = round(debug1 - CUBE_SIZE / 4)
-    # debug3 = map_to_new_range(debug2, -GRID_SIZE/2, GRID_SIZE/2, GRID_SIZE-1, 0)
-    # debug4 = math.floor(debug3)
-    # debug5 = int(debug4)
-
-    # print("-------------")
-    # print(f"debug ray[2] : {ray[2]}")
-    # print(f"debug t : {t}")
-    # print(f"debug1 : {debug1}")
-    # print(f"debug2 : {debug2}")
-    # print(f"debug3 : {debug3}")
-    # print(f"debug4 : {debug4}")
-    # print(f"debug5 : {debug5}")
-    # print("-------------")
-    #debug
-
 
     # Map grid positions
     grid_x = map_to_new_range(grid_x, -GRID_SIZE/2, GRID_SIZE/2, GRID_SIZE-1, 0)
@@ -282,7 +276,7 @@ def get_grid_position(mouse_x, mouse_y):
         grid_x = math.floor(grid_x)
         grid_z = math.floor(grid_z)
 
-        grid_x = int(grid_x) 
+        grid_x = int(grid_x)
         grid_y = int(grid_y)
         grid_z = int(grid_z)
     
@@ -316,20 +310,11 @@ def OnMouseClick(button, state, x, y):
     else:
         clicked = False
 
-# draw mode
-draw_mode_num = 1
-draw_mode = "Add"
-
-def drawMode(num):
-    if num == 0:
-        drawMode = "Delete"
-    elif num == 1:
-        drawMode = "Add"
-    return drawMode
-#draw mode
-
-
-
+def place_block(x, y, z):
+    if grid[grid_pos[0], grid_pos[1], grid_pos[2]] == 0:  # Place block
+        grid[grid_pos[0], grid_pos[1], grid_pos[2]] = 1
+    else:  # Remove block
+        grid[grid_pos[0], grid_pos[1], grid_pos[2]] = 0
 #--------------------Ray Casting 3D Selection----------------------
 
 camera = 0
@@ -338,27 +323,36 @@ sketch_view = "front"
 
 # Keyboard interaction handler
 def key_press(key, x, y):
-    global cursor_y, camera, layer, draw_mode_num
+    global cursor_x, cursor_y, cursor_z, camera, layer
     key = key.decode("utf-8").lower()
 
-    #camera control
-    if key == 'q':
-        camera += 1
-    elif key == 'e':
-        camera -= 1
-
-    #layer control
+    if key == 'w':  # Move north
+        cursor_z = max(0, cursor_z - 1)
+    elif key == 's':  # Move south
+        cursor_z = min(GRID_SIZE - 1, cursor_z + 1)
+    elif key == 'a':  # Move west
+        cursor_x = max(0, cursor_x - 1)
+    elif key == 'd':  # Move east
+        cursor_x = min(GRID_SIZE - 1, cursor_x + 1)
     elif key == 'z':  # Down
         #cursor_y = min(GRID_SIZE - 1, cursor_y - 1)
         layer = min(GRID_SIZE - 1, layer - 1)
     elif key == 'c':  # Up
         #cursor_y = min(GRID_SIZE - 1, cursor_y + 1)
         layer = min(GRID_SIZE - 1, layer + 1)
-
-    #draw mode control (toggle)
-    elif key == 'w':
-        draw_mode_num += 1
-        draw_mode_num = draw_mode_num % 2
+    elif key == '\r':  # Enter key to place/remove block
+        if grid[cursor_x, cursor_y, cursor_z] == 0:  # Place block
+            grid[cursor_x, cursor_y, cursor_z] = 1
+        else:  # Remove block
+            grid[cursor_x, cursor_y, cursor_z] = 0
+    # Button ">"
+    elif key == 'q':
+        camera += 1
+        #SelectCamera(camera)
+    # Button "<"
+    elif key == 'e':
+        camera -= 1
+        #SelectCamera(camera)
 
     # Save grid to slot
     elif key == '1':
@@ -436,9 +430,9 @@ def display_ortho():
 
     glutSwapBuffers()
 
-#--------------------Main Display----------------------------
+# Display callback
 def display():
-    global eye_position, new_eye_position, draw_mode_num
+    global eye_position, new_eye_position
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
     SelectCamera(camera)
@@ -446,15 +440,15 @@ def display():
     gluLookAt(eye_position[0], eye_position[1], eye_position[2], 0, 0, 0, 0, 1, 0)
 
     draw_reference_line()
-
     draw_block_function()
-
-    
 
     # Draw text
     glColor3f(1.0, 1.0, 1.0)
+    #draw_text(10, window_height - 20, f"Cursor: ({cursor_x}, {cursor_y}, {cursor_z})")
     draw_text(10, window_height - 40, f"Mouse: ({mouse_x}, {mouse_y}), Grid Pos: {grid_pos}, Draw Layer: {layer}")
-    draw_text(10, window_height - 60, f"Draw mode : {drawMode(draw_mode_num)}")
+    #draw_text(10, window_height - 60, "Controls: W (North), S (South), A (West), D (East), Enter (Place/Remove Block)")
+    #draw_text(10, window_height - 120, f"View : {camera} pos : {eye_position}")
+    
     draw_text(10, window_height - 80, f"Save Slot : {current_slot}")
     
     if file_status == "saved":
@@ -467,68 +461,14 @@ def display():
     draw_text(window_width - 300, window_height - 20, "Press '1', '2', '3' to Save to Slots")
     draw_text(window_width - 300, window_height - 40, "Press '4', '5', '6' to Load from Slots")
 
-    draw_text(window_width - 630, 0 + 0, "Pree W to toggle between Drawing modes")
     draw_text(window_width - 630, 0 + 20, "Press Q or E to Rotate Camera Counter Clockwise or Clockwise")
     draw_text(window_width - 630, 0 + 40, "Press Z or C to Change Vertical Drawing Layer down 1 layer or up 1 layer")
 
+
+    
     glutSwapBuffers()
 
-
-
-#--------------------Main Display----------------------------
-
-#--------------------Ortho----------------------------
-# Draw block in orthographic projection
-def draw_block_ortho():
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    aspect_ratio = (window_width // 2) / (window_height // 2)
-    if aspect_ratio > 1:
-        glOrtho(-GRID_SIZE * aspect_ratio, GRID_SIZE * aspect_ratio, -GRID_SIZE, GRID_SIZE, -GRID_SIZE, GRID_SIZE)
-    else:
-        glOrtho(-GRID_SIZE, GRID_SIZE, -GRID_SIZE / aspect_ratio, GRID_SIZE / aspect_ratio, -GRID_SIZE, GRID_SIZE)
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
-
-    # Front view
-    glViewport(0, window_height // 2, window_width // 2, window_height // 2)
-    glLoadIdentity()
-    draw_block_function()
-    draw_text(10, window_height // 2 + 20, "Front View")
-
-    # Top view
-    glViewport(window_width // 2, window_height // 2, window_width // 2, window_height // 2)
-    glLoadIdentity()
-    glRotatef(90, 1, 0, 0)
-    draw_block_function()
-    draw_text(window_width // 2 + 10, window_height // 2 + 20, "Top View")
-
-    # Left side view
-    glViewport(0, 0, window_width // 2, window_height // 2)
-    glLoadIdentity()
-    glRotatef(90, 0, 1, 0)
-    draw_block_function()
-    draw_text(10, window_height // 2 - window_height // 2 + 20, "Left Side View")
-
-    # Right side view
-    glViewport(window_width // 2, 0, window_width // 2, window_height // 2)
-    glLoadIdentity()
-    glRotatef(-90, 0, 1, 0)
-    draw_block_function()
-    draw_text(window_width // 2 + 10, window_height // 2 - window_height // 2 + 20, "Right Side View")
-
-    # Save block data to 3D list
-    for x in range(GRID_SIZE):
-        for y in range(GRID_SIZE):
-            for z in range(GRID_SIZE):
-                if is_block_present(x, y, z):
-                    grid[x][y][z] = 1
-
-
-
-#--------------------Ortho----------------------------
-
-# Driver function
+# Main function
 def main():
     glutInit()
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
